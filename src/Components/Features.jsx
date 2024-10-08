d076583dbebb3e85d031420d00b589a1b4b72537
MuzzammilAfridi <118999159+MuzzammilAfridi@users.noreply.github.com>
2024-10-08 12:45:40 +0530

    Images path is corrected

--- src/Components/Features.jsx
+++ src/Components/Features.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 const Features = () => {
   return (
     <div className='mx-5  flex flex-wrap sm:items-center sm:justify-center gap-5 my-5'>
-        <img className='rounded-lg sm:w-[28vw]' src=".\images\fet-01.jpg" alt="img" />
-        <img className='rounded-lg  sm:w-[28vw]' src=".\images\fet-02.jpg" alt="img" />
-        <img className='rounded-lg sm:w-[28vw]' src=".\images\fet-03.jpg" alt="img" />
-        <img className='rounded-lg sm:w-[28vw]' src=".\images\fet-04.jpg" alt="img" />
-        <img className='rounded-lg sm:w-[28vw]' src=".\images\fet-05.jpg" alt="img" />
-        <img className='rounded-lg sm:w-[28vw]' src=".\images\fet-05.jpg" alt="img" />
+        <img className='rounded-lg sm:w-[28vw]' src="./images/fet-01.jpg" alt="img" />
+        <img className='rounded-lg  sm:w-[28vw]' src="./images/fet-02.jpg" alt="img" />
+        <img className='rounded-lg sm:w-[28vw]' src="./images/fet-03.jpg" alt="img" />
+        <img className='rounded-lg sm:w-[28vw]' src="./images/fet-04.jpg" alt="img" />
+        <img className='rounded-lg sm:w-[28vw]' src="./images/fet-05.jpg" alt="img" />
+        <img className='rounded-lg sm:w-[28vw]' src="./images/fet-05.jpg" alt="img" />
        
       
     </div>
